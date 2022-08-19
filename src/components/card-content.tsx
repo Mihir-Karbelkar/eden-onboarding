31560ee908af73e23455f194b252836f0dcb6801
Mihir Karbelkar <karbelkar.mihir@gmail.com>
2022-08-20 01:35:59 +0530

    Helper component added

---
+++ src/components/card-content.tsx
@@ -0,0 +1,59 @@
+import { Box, Text } from '@chakra-ui/react';
+import { RiUserFill, RiTeamFill } from 'react-icons/ri';
+import { PURPLE, TEXT_GRAY } from '../constants';
+import { UsageRadioOptionsType } from '../types/component-types';
+
+const CardContent = (props: {
+  value: UsageRadioOptionsType;
+  isChecked: boolean | undefined;
+  isMobile: boolean;
+}) => {
+  const { value, isChecked, isMobile } = props;
+  switch (value) {
+    case 'individual':
+      return (
+        <Box>
+          <Box
+            display="flex"
+            alignItems={isMobile ? 'center' : 'start'}
+            flexDirection="column"
+          >
+            <RiUserFill
+              size={isMobile ? '2em' : '1.5em'}
+              color={isChecked ? PURPLE : 'black'}
+            />
+          </Box>
+
+          <Text mt={4} fontWeight="bold">
+            For myself
+          </Text>
+          <Text color={TEXT_GRAY} mt={4} fontSize="medium">
+            Write better. Think more clearly. Stay organized
+          </Text>
+        </Box>
+      );
+    case 'team':
+      return (
+        <Box>
+          <Box
+            display="flex"
+            alignItems={isMobile ? 'center' : 'start'}
+            flexDirection="column"
+          >
+            <RiTeamFill
+              size={isMobile ? '2em' : '1.5em'}
+              color={isChecked ? PURPLE : 'black'}
+            />
+          </Box>
+          <Text mt={4} fontWeight="bold">
+            With my team
+          </Text>
+          <Text color={TEXT_GRAY} mt={4} fontSize="medium">
+            Wikis, docs, tasks & projects, all in one place.
+          </Text>
+        </Box>
+      );
+  }
+};
+
+export default CardContent;
---
+++ src/components/usage-radio-card.tsx
@@ -0,0 +1,31 @@
+import { Box, useRadio, UseRadioProps } from '@chakra-ui/react';
+import { PURPLE } from '../constants';
+import { UsageRadioCardType } from '../types/component-types';
+
+const UsageRadioCard = (
+  props: React.PropsWithChildren<UsageRadioCardType & UseRadioProps>
+) => {
+  const { cardCSS } = props;
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+  return (
+    <Box as="label" style={cardCSS}>
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        _checked={{
+          borderColor: PURPLE,
+        }}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+};
+
+export default UsageRadioCard;
